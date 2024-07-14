77c97eefeebdeae5c4aedfdaa1a250559a17e365
USER <mohamedmezoo210443@gmail.com>
2024-07-14 16:02:14 +0300

    last commit

--- my-app/src/components/Bar.jsx
+++ my-app/src/components/Bar.jsx
@@ -53,22 +53,17 @@ const Bar = () => {
     <>
     <div className={`bar-container ${fixed?"fixed":""}`}>
       <Swiper slidesPerView={9} spaceBetween={25} breakpoints={{
-           300:{slidesPerView:1.2},
-           400:{slidesPerView:1.4},
-           500:{slidesPerView:1.8},
-           600:{slidesPerView:4.5},
-           700:{slidesPerView:2.5},
-           800:{slidesPerView:2.9},
-           900:{slidesPerView:2.1},
-           1050:{slidesPerView:2.5},
-           1180:{slidesPerView:2.6},
-           1300:{slidesPerView:3},
-           1400:{slidesPerView:3.3},
-           1550:{slidesPerView:3.5},
-           1650:{slidesPerView:3.9},
-           1850:{slidesPerView:4.1},
-           1950:{slidesPerView:4.3},
-           2100:{slidesPerView:4.9},
+           300:{slidesPerView:3.4},
+           400:{slidesPerView:3.9},
+           500:{slidesPerView:4.8},
+           600:{slidesPerView:5.7},
+           700:{slidesPerView:6.5},
+           800:{slidesPerView:7.5},
+           900:{slidesPerView:8.5},
+           1000:{slidesPerView:11},
+        
+        
+        
 
       }} >
         <SwiperSlide >
