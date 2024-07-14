202a695745614152588138608d10f4a7500f826f
USER <mohamedmezoo210443@gmail.com>
2024-07-14 18:23:53 +0300

    last commit

--- my-app/src/components/Branches.jsx
+++ my-app/src/components/Branches.jsx
@@ -82,22 +82,22 @@ const Branches = () => {
      <main className='offers' >
       <h3 id='offers'>offers</h3>
       <Swiper slidesPerView={3.5} spaceBetween={20} loop={true} breakpoints={{
-        300:{slidesPerView:1.2},
-        400:{slidesPerView:1.4},
-        500:{slidesPerView:1.8},
-        600:{slidesPerView:2.1},
-        700:{slidesPerView:2.5},
-        800:{slidesPerView:2.9},
-        900:{slidesPerView:2.1},
-        1050:{slidesPerView:2.5},
-        1180:{slidesPerView:2.6},
-        1300:{slidesPerView:3},
-        1400:{slidesPerView:3.3},
-        1550:{slidesPerView:3.5},
-        1650:{slidesPerView:3.9},
-        1850:{slidesPerView:4.1},
-        1950:{slidesPerView:4.3},
-        2100:{slidesPerView:4.9},
+       300:{slidesPerView:1.2},
+       400:{slidesPerView:1.4},
+       500:{slidesPerView:1.8},
+       600:{slidesPerView:2.1},
+       700:{slidesPerView:2.5},
+       800:{slidesPerView:2.9},
+       900:{slidesPerView:2.1},
+       1050:{slidesPerView:2.5},
+       1180:{slidesPerView:2.6},
+       1300:{slidesPerView:3},
+       1400:{slidesPerView:3.3},
+       1550:{slidesPerView:3.5},
+       1650:{slidesPerView:3.9},
+       1850:{slidesPerView:4.1},
+       1950:{slidesPerView:4.3},
+       2100:{slidesPerView:4.9},
 
       }}>
         {Offerss?.data?.map((Data,id)=><>
