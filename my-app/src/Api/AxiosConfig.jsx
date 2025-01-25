886ee97f91060684d256832f2d32c400069a680b
USER <mohamedmezoo210443@gmail.com>
2025-01-25 15:06:23 +0200

    last commit

--- my-app/src/Api/AxiosConfig.jsx
+++ my-app/src/Api/AxiosConfig.jsx
@@ -5,7 +5,7 @@
 import axios from "axios"
 
 // const Base_url="http://localhost:1337/api"
-const Base_url="https://lovely-fitness-6cabe5f596.strapiapp.com/api"
+const Base_url="https://victorious-dog-e13704a331.strapiapp.com/api"
 // eslint-disable-next-line react-refresh/only-export-components
 const AxiosConfig=axios.create({
     baseURL:Base_url,
